0aba38a041f233c35c9d6f3263325ed06d262854
ian gacheru <gacheruian99@gmail.com>
2024-01-10 19:28:39 +0300

    test

--- list.py
+++ list.py
@@ -4,7 +4,7 @@ List1 = [8, 9, 3,6, 1, 101]
 List1.reverse()
 print("The reversed list is", List1)
 
-List2 = [91, 67, 120, 34, 76, 54, 78, 87, 56, 54, 345]
+List2 = [91, 67, 120, 34, 76, 54, 78, 87, 56, 345]
 List2.sort()
 print ("The sorted list is", List2)
 
